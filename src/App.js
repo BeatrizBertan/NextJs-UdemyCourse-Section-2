6423ad3582a37358a36a1d6936d0805d5b4798c3
BeatrizBertan <61274318+BeatrizBertan@users.noreply.github.com>
2021-05-02 12:38:02 -0300

    Projeto

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Route, Switch} from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetups';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout'
+
+function App() {
+  // localhost:3000/favorites
+  //my-page.com/
+  return(
+    <Layout>
+      <Switch>
+        <Route path = '/' exact = {true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path = '/new-meetup'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path = '/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
